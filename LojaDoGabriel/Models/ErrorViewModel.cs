efb7326375203f1258f563f8b6fa2c8d7e73f0be
GabrielMathiasBernrado <fcbgabrielmathias@gmail.com>
2025-02-03 15:01:48 -0300

    Criado o Projeto MVC

---
+++ LojaDoGabriel/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LojaDoGabriel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
